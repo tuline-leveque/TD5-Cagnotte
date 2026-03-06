a84aa94ce787165da97362abdc448cfed9b6f385
tuline-leveque <tuline.leveque@gmail.com>
2026-03-06 08:31:36 +0100

    routes + App.vue

--- td-cagnotte/src/main.js
+++ td-cagnotte/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import MyPlugin from "@/plugins/tools.js";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(MyPlugin)
 
 app.mount('#app')
--- td-cagnotte/src/router/index.js
+++ td-cagnotte/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Cagnottes from '../views/Cagnottes.vue'
+import CreateCagnotte from '../views/CreateCagnotte.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: 'router', redirect: "/cagnottes" },
+    { path: '/cagnottes', name: 'home', component: Cagnottes },
+    { path: '/cagnottes/new', name: 'cagnotte-create', component: CreateCagnotte },
+  ],
 })
 
 export default router
